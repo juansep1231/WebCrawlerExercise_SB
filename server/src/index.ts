996b1dd33d6ffdff1f56815ce1dcf850da90adf1
Juan Posso <juan.posso@epn.edu.ec>
2024-09-19 13:50:12 -0500

    feat: add scrapping endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,11 @@
 import express from 'express';
+import { scrapeHackerNews } from './controller/scraper';
 
 
 const app = express();
+
+app.get('/entries', async (req, res) => {
+    const entries = await scrapeHackerNews();
+    res.json(entries);
+});
 app.listen(3001, () => console.log('Server running on port 3001'));
