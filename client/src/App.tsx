86004571f259375d1fbf4cfd138e88ad650d90c2
Juan Posso <juan.posso@epn.edu.ec>
2024-09-20 01:54:45 -0500

    chore: remove unused imports

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState} from 'react';
 import './App.css';
 import EntryList from './components/EntryList';
 import { useFetchEntries } from './hooks/fetchEntriesHook';
