cf9bf4b1a02a836c9ef4f24d86e53ae911c9dc9a
Juan Posso <juan.posso@epn.edu.ec>
2024-09-20 12:37:10 -0500

    test: add unit test for filter functions

---
+++ server/tests/fixtures/entriesData.ts
@@ -0,0 +1,109 @@
+import { Entry } from "../../src/models/types";
+
+export const entriesData: Entry[] = [
+  {
+    number: 1,
+    title: "Sipeed NanoKVM: A RISC-V Stick-On for Embedded Devices",
+    points: 220,
+    comments: 47,
+  },
+  {
+    number: 2,
+    title: "AI: Markets for Lemons, and the Great Logging Off (2022)",
+    points: 345,
+    comments: 59,
+  },
+  {
+    number: 3,
+    title: "OpenAI GPT-4: A Comprehensive Analysis and Future Projections",
+    points: 512,
+    comments: 104,
+  },
+  {
+    number: 4,
+    title: "Quantum Networking: Linking Qubits Over Long Distances (2023)",
+    points: 429,
+    comments: 88,
+  },
+  {
+    number: 5,
+    title: "Linux Kernel 5.15: A Look at the Latest Long-Term Support Release",
+    points: 398,
+    comments: 63,
+  },
+
+  {
+    number: 6,
+    title: "Rust-158 Released with Improvements Measure",
+    points: 621,
+    comments: 78,
+  },
+
+  {
+    number: 7,
+    title: "Show HN: Distributed Web",
+    points: 150,
+    comments: 29,
+  },
+  {
+    number: 8,
+    title: "FPGA-based Neural Networks",
+    points: 517,
+    comments: 73,
+  },
+];
+
+export const entriesDataLessThanFive: Entry[] = [
+  {
+    number: 6,
+    title: "Rust-158 Released with Improvements Measure",
+    points: 621,
+    comments: 78,
+  },
+
+  {
+    number: 7,
+    title: "Show HN: Distributed Web",
+    points: 150,
+    comments: 29,
+  },
+  {
+    number: 8,
+    title: "FPGA-based Neural Networks",
+    points: 517,
+    comments: 73,
+  },
+];
+
+export const entriesDataMoreThanFive: Entry[] = [
+  {
+    number: 1,
+    title: "Sipeed NanoKVM: A RISC-V Stick-On for Embedded Devices",
+    points: 220,
+    comments: 47,
+  },
+  {
+    number: 2,
+    title: "AI: Markets for Lemons, and the Great Logging Off (2022)",
+    points: 345,
+    comments: 59,
+  },
+  {
+    number: 3,
+    title: "OpenAI GPT-4: A Comprehensive Analysis and Future Projections",
+    points: 512,
+    comments: 104,
+  },
+  {
+    number: 4,
+    title: "Quantum Networking: Linking Qubits Over Long Distances (2023)",
+    points: 429,
+    comments: 88,
+  },
+  {
+    number: 5,
+    title: "Linux Kernel 5.15: A Look at the Latest Long-Term Support Release",
+    points: 398,
+    comments: 63,
+  },
+];
