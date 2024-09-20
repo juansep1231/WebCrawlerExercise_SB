09d27a3a17fa6817e4c1520a675b2ce671da550b
Juan Posso <juan.posso@epn.edu.ec>
2024-09-20 11:37:19 -0500

    fix: change regular expression for point and comment, anad and export to functions

--- server/src/controller/scraper.ts
+++ server/src/controller/scraper.ts
@@ -2,29 +2,29 @@ import axios from 'axios';
 import * as cheerio from 'cheerio';
 import { Entry } from '../models/types.ts';
 
-function extractNumber(text: string, regex: RegExp): number {
+export function extractNumber(text: string, regex: RegExp): number {
   const match = text.match(regex);
   return match ? parseInt(match[1]) : 0;
 }
 
-function getNumber(element: cheerio.Cheerio): number {
+export function getNumber(element: cheerio.Cheerio): number {
   const numberOrder = element.find('.rank').text();
   const number = extractNumber(numberOrder, /(\d+)\./); 
   return number;
 }
 
-function getTitle(element: cheerio.Cheerio): string {
+export function getTitle(element: cheerio.Cheerio): string {
   return element.find('.titleline > a').text();
 }
 
-function getPoints(element: cheerio.Cheerio): number {
+export function getPoints(element: cheerio.Cheerio): number {
   const subtext = (element).next().find('.subtext');
   const pointsText = subtext.find('.score').text();
   const points = extractNumber(pointsText, /(\d+)\s+points/); 
   return points;
 }
 
-function getComments(element: cheerio.Cheerio): number {
+export function getComments(element: cheerio.Cheerio): number {
   const subtext = (element).next().find('.subtext');
   const commentsText = subtext.find('a').last().text();
   const comments = extractNumber(commentsText, /(\d+)\scomment/);  
@@ -44,7 +44,6 @@ export async function scrapeHackerNews(): Promise<Entry[]> {
       const title = getTitle($(element));
       const points = getPoints($(element));
       const comments = getComments($(element));
-  
       entries.push({ number, title, points, comments });
     });
     
